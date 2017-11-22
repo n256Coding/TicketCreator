ee8ab52d482ec8d48a2edc33a813de449c798405
MY-HP-PROBOOK-4\SM_MYPC <sampathruwansm@gmail.com>
2017-11-22 15:56:39 +0530

    design layouts

--- app/src/main/java/com/csse/ticketcreator/HomeActivity.java
+++ app/src/main/java/com/csse/ticketcreator/HomeActivity.java
@@ -1,20 +1,20 @@
 package com.csse.ticketcreator;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-    ImageButton imageButton;
+    Button imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        imageButton = (ImageButton) findViewById(R.id.btnNewCard);
+        imageButton = (Button) findViewById(R.id.btnNewCard);
 
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
